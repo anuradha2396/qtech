19b71b2a44e123fbc0ef0297f18f9badb06d15a6
anuradha2396 <anuradha.zapbuild@gmail.com>
2020-05-01 11:14:30 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-	belongs_to: result
-	has_many: exam_schedule
-	validates :email, uniqueness: true
+	
+	has_many :exam_schedule
+	validates :email, uniqueness: true, presence: true
 	validates :first_name, :last_name,  presence: true
 	validates :password, length: { in: 6..20 }
-    validates :phone_no, length: { in: 10..20 }
+    
 
 end
